dbefaceab75daaccd6e4444b25f014722c93a135
Daniel <gilberttitah@gmail.com>
2024-12-14 11:08:42 +0300

    dsf

--- events/events/settings.py
+++ events/events/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4+5x98mjmabxf+0(!s#jptm=wj6$=8rt48s&ai0_8n7@krxeox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
